abc7268cea61649c642eb55d0ed13b01dd83140b
Thomas Schmidt <71519368+thomassilva23@users.noreply.github.com>
2023-04-13 16:56:38 -0300

    Conexão com mongodb

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # reactgram
+
 node version 16.6.2
+
+para fazer requisições para o mongodb, precisa adicionar o ip adress no projeto
--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Conf JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// DB Connection
+require("./config/db.js");
+
+//routes
+const router = require("./routes/Router.js");
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App rodando na porta ${port}`);
