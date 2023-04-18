ba0617452148fdc520a1ba06d3d9d530918ce6e3
thomassilva23 <thomassilva23@hotmail.com>
2023-04-18 16:55:55 -0300

    Criação de validação de autenticação

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS;
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.yitqki2.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.wgdaapq.mongodb.net/?retryWrites=true&w=majority`
     );
 
     console.log("Conectou ao banco!");
